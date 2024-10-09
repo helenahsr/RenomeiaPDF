aad7a97ed262f8e056070ef25baf247ac2aa59b7
Helena Santos Rezende <99421906+helenahsr@users.noreply.github.com>
2024-10-09 11:00:12 -0300

    Projeto comentado e com interface

---
+++ Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace RenomeiaPDF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnEscolhePasta = new System.Windows.Forms.Button();
+            this.txtDiretorio = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnRodar = new System.Windows.Forms.Button();
+            this.lstLog = new System.Windows.Forms.ListBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            // 
+            // btnEscolhePasta
+            // 
+            this.btnEscolhePasta.Location = new System.Drawing.Point(13, 50);
+            this.btnEscolhePasta.Name = "btnEscolhePasta";
+            this.btnEscolhePasta.Size = new System.Drawing.Size(371, 29);
+            this.btnEscolhePasta.TabIndex = 0;
+            this.btnEscolhePasta.Text = "Escolher Pasta";
+            this.btnEscolhePasta.UseVisualStyleBackColor = true;
+            this.btnEscolhePasta.Click += new System.EventHandler(this.btnEscolhePasta_Click);
+            // 
+            // txtDiretorio
+            // 
+            this.txtDiretorio.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDiretorio.Location = new System.Drawing.Point(13, 13);
+            this.txtDiretorio.Name = "txtDiretorio";
+            this.txtDiretorio.Size = new System.Drawing.Size(371, 24);
+            this.txtDiretorio.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnRodar);
+            this.panel1.Controls.Add(this.txtDiretorio);
+            this.panel1.Controls.Add(this.btnEscolhePasta);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(404, 127);
+            this.panel1.TabIndex = 2;
+            // 
+            // btnRodar
+            // 
+            this.btnRodar.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnRodar.Location = new System.Drawing.Point(13, 87);
+            this.btnRodar.Name = "btnRodar";
+            this.btnRodar.Size = new System.Drawing.Size(371, 29);
+            this.btnRodar.TabIndex = 2;
+            this.btnRodar.Text = "Rodar";
+            this.btnRodar.UseVisualStyleBackColor = false;
+            this.btnRodar.Click += new System.EventHandler(this.btnRodar_Click);
+            // 
+            // lstLog
+            // 
+            this.lstLog.FormattingEnabled = true;
+            this.lstLog.Location = new System.Drawing.Point(12, 145);
+            this.lstLog.Name = "lstLog";
+            this.lstLog.Size = new System.Drawing.Size(404, 290);
+            this.lstLog.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 450);
+            this.Controls.Add(this.lstLog);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "RenomeiaPDF";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btnEscolhePasta;
+        private System.Windows.Forms.TextBox txtDiretorio;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnRodar;
+        private System.Windows.Forms.ListBox lstLog;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using EasyAutomationFramework;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace RenomeiaPDF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEscolhePasta_Click(object sender, EventArgs e)
+        {
+            ChooseFolder();
+        }
+
+        public void ChooseFolder()
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtDiretorio.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void btnRodar_Click(object sender, EventArgs e)
+        {
+            //Pegando o diretório
+            string diretorio = txtDiretorio.Text; //Pega o diretório escolhido
+            int cont = 0; //Contador para o número de arquivos inválidos
+            DirectoryInfo dir = new DirectoryInfo(diretorio); //Cria um objeto para representar o diretório
+            FileInfo[] arquivosPdf = dir.GetFiles("*.pdf"); // Lista todos os arquivos .pdf no diretório informado
+
+            foreach (FileInfo arquivo in arquivosPdf) //Foreach para percorrer cada arquivo
+            {
+                string caminho = "file:///" + arquivo.FullName; // Adicionando o file:// ao caminho do arquivo para o pdf poder ser lido pelo framework
+
+                try
+                {
+                    var text = Base.ExtractTextPdf(caminho); //Extraindo todo o texto do arquivo
+                    caminho = new Uri(caminho).LocalPath; //Retirando o file:// para podermos renomea-lo depois
+                    var placa = text.IndexOf("Placa:"); //Procurando o campo placa
+                    var teste = text.Substring(placa + 7, 7); // Pegando a placa
+                    string padraoPlaca = @"^[A-Z]{3}\d{4}$"; // Criando o padrão de placa antiga
+                    string padrao2 = "^[A-Z]{3}\\d[A-Z]\\d{2}$"; // Criando o padrão de placa atual (Mercosul)
+                    if (Regex.IsMatch(teste, padraoPlaca) || Regex.IsMatch(teste, padrao2)) // Testando se o que foi pego está no padrão, ou seja, testando se é uma placa
+                    {
+                        var newNome = teste + ".pdf"; //Caso sim, setaremos o novo nome com o valor da placa mais o ".pdf"
+                        File.Move(caminho, Path.Combine(Path.GetDirectoryName(caminho), newNome)); //Encontrando o arquivo e o renomeando.
+                    }
+                    else
+                    {
+                        lstLog.Items.Add($"O documento {arquivo} não contém placa ou a placa não está no formato válido."); //Caso não, informaremos que não foi possível
+                        cont++; // Adicionaremos mais um ao contador
+                        var newNome = $"invalido{cont}.pdf"; // setando o novo nome do arquivo sem placa ou inválido
+                        File.Move(caminho, Path.Combine(Path.GetDirectoryName(caminho), newNome)); // Encontrando e renomeando o arquivo
+                    }
+
+                }
+                catch (Exception ex) // Caso haja problemas na leitura de algum arquivo
+                {
+                    lstLog.Items.Add($"Erro ao processar o arquivo {arquivo.Name}: {ex.Message}");
+                }
+            }
+
+            //Terminando o programa
+            lstLog.Items.Add("\nConcluído!");
+        }
+    }
+}
